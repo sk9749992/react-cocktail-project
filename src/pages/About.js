bb5e454e0b3e26fc591a99d6513be56972cf51a1
sk9749992 <sk9749992@gmail.com>
2022-04-13 11:03:34 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ab magni, 
+        quod quia doloribus odit? Fuga corrupti eos veritatis soluta expedita molestias quis aperiam
+        eaque aut reiciendis repellendus, error, excepturi cumque voluptas? Mollitia accusamus temporibus
+        distinctio voluptatum quos tempore sit.
+      </p>
+    </section>
+  )
+}
+
+export default About
